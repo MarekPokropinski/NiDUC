61363a146134b992d9c8e657821053128102a371
Marek Pokropiński <maro900@op.pl>
2018-03-27 22:59:12 +0200

    Zmieniono model klasy na classdef

--- @transmitter/createBracket.m
+++
@@ -1,7 +0,0 @@
-function p = createBracket(obj,packets,fun)
-  brackets = [];
-  for packet= packets
-    brackets = cat(2,brackets,fun(packet));    
-  endfor
-  p = cat(1,brackets,packets);
-endfunction
