370838cbc722fee9e848deac89b748da478e8c2c
Marek Pokropiński <maro900@op.pl>
2018-03-27 23:54:15 +0200

    Zmiana klasy na classdef

--- Transmitter/@data_generator/saveToFile.m
+++
@@ -1,5 +0,0 @@
-function saveToFile(filename,vector)
-  f = fopen(filename,"wb");
-  fwrite(f,vector,precision="uint8");
-  fclose(f);
-endfunction
---
+++ Transmitter/DataGenerator.m
@@ -0,0 +1,39 @@
+classdef DataGenerator < handle
+  properties(SetAccess = protected)
+    self.seed=0;
+  endproperties
+  
+  methods
+    function self = DataGenerator(seed)
+        if (nargin==0)
+           self.seed = 0;
+        elseif (nargin==1)
+           self.seed = seed;
+        endif
+    endfunction
+     
+    function p = getVector(obj,size)
+      if (obj.seed!=0)
+        rand("seed",obj.seed);
+      else
+        rand("state","reset");
+      endif
+      
+      p = randi([0 1],size,1,class="uint8");
+    endfunction
+    
+    function vector = loadFromFile(self,filename)
+      f=fopen(filename,'rb');
+      vector=fread(f,precision="uint8");
+      fclose(f);
+    endfunction
+
+    function saveToFile(self,filename,vector)
+      f = fopen(filename,"wb");
+      fwrite(f,vector,precision="uint8");
+      fclose(f);
+    endfunction
+     
+  end
+  
+end
