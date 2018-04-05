7084e537ad1599ed45ee838a132e83525e7356d6
Marek Pokropiński <maro900@op.pl>
2018-04-05 22:22:21 +0200

    Dodano obsługę systemu stop and wait

--- Transmitter/Transmitter.m
+++ Transmitter/Transmitter.m
@@ -1,12 +1,74 @@
 classdef Transmitter < handle
   properties(SetAccess = protected)
     packet_size
+    type
+    data
+    channel
+    timeout    
+    state
   endproperties
+  
 
   methods (Access = public)
-    function self = Transmitter(Packet_size)
-      self.packet_size=Packet_size;
+    
+    function update(self,deltaTime)
+      
+      if self.type=='saw'
+        self.stop_and_wait(deltaTime)
+      endif      
+    endfunction
+    
+    function stop_and_wait(self,deltaTime)
+      self.state
+      if strcmp(self.state , 'sending')        
+        if length(self.data)==0
+          self.state = 'end';
+          break;
+        endif
+                
+        pack = self.data(:,1);
+        for i=1:length(pack)          
+          self.channel.send(pack(i));
+        endfor
+        
+        self.data=self.data(:,2:end);        
+        self.state = 'wait';
+        
+        
+      elseif strcmp(self.state , 'wait')
+        if self.channel.getACK()
+          self.state = 'sending';
+        endif
+        
+      elseif strcmp(self.state , 'end')
+        if length(self.data)>0
+          self.state = 'sending';
+        endif
+      endif      
+      
+    endfunction
+    
+    function init(self)
+      
+      if strcmp(self.type,'saw')
+        self.state = 'sending';
+      endif      
+    endfunction
+  
+    function self = Transmitter(packet_size,channel,type,timeout)
+      self.packet_size=packet_size;
+      self.type=type;
+      self.channel=channel;
+      self.timeout=timeout;
+      
+      self.init();      
     end
+    
+    function send(self,data,method)
+      self.data=data;      
+      self.data=self.createBracket(self.createPackets(data),method);
+    endfunction
+    
     
     function packets = createPackets(self,data)
       padding = mod(self.packet_size-mod(rows(data),self.packet_size),self.packet_size);
@@ -20,7 +82,7 @@ classdef Transmitter < handle
            
       
       for packet= packets
-        if method=='crc'
+        if strcmp(method,'crc')
           brackets = cat(2,brackets,Transmitter.get_crc(packet)); 
         else
           brackets = cat(2,brackets,Transmitter.get_xor(packet)); 
@@ -51,6 +113,6 @@ classdef Transmitter < handle
         endif
       endfor
       p=d(len+1:len+length(a)-1);
-    endfunction      
+    endfunction  
   end
 end
